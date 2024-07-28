4549ad092b93fa5c50aafd4059af2b8344df6486
Efaideleon <efaideleon@gmail.com>
2024-07-28 14:08:06 -0700

    Design Patterns

---
+++ Design Patterns/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns
+{
+    public class StrategyPattern
+    {
+        public static void PrintSomething()
+        {
+            Console.WriteLine("From strategy");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DesignPatterns.StrategyPattern.PrintSomething();
+    }
+}
+
